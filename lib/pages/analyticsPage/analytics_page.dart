bf814899eed3b6189010f16667f548a9e88f7daa
yudate <y0303noki@gmail.com>
2021-09-15 19:39:40 +0900

    アナリティクス画面修正

--- lib/pages/analyticsPage/analytics_page.dart
+++ lib/pages/analyticsPage/analytics_page.dart
@@ -88,150 +88,164 @@ class AnalyticsPage extends StatelessWidget {
       }
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.black,
+    return GestureDetector(
+      // 水平方向にスワイプしたら画面を戻す
+      onHorizontalDragUpdate: (details) {
+        if (details.delta.dx > 18 || details.delta.dx < -18) {
+          Navigator.pop(context);
+        }
+      },
+      // 垂直方向にスワイプしたら画面を戻す
+      onVerticalDragUpdate: (details) {
+        // if (details.delta.dy > 25 || details.delta.dy < -25) {
+        //   Navigator.pop(context);
+        // }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: const IconThemeData(
+            color: Colors.black,
+          ),
+          elevation: 0,
+          backgroundColor: Colors.white,
         ),
-        elevation: 0,
-        backgroundColor: Colors.white,
-      ),
-      body: Consumer<AnalyticsProvider>(builder: (ctx, model, _) {
-        return Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      child: DropdownButton<String>(
-                        value: model.selectedYear.toString(),
-                        onChanged: (String? selectYear) {
-                          if (selectYear == null) {
-                            return;
-                          }
-                          model.setSelectedYear(int.parse(selectYear));
-                          DateTime now = DateTime.now();
-                          model.init(
-                              DateTime(int.parse(selectYear), now.month, 1));
-                        },
-                        items: _yearList
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                    Container(
-                      child: Text('年'),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Container(
-                      child: DropdownButton<String>(
-                        value: model.selectedMonth.toString(),
-                        onChanged: (String? selectedMonth) {
-                          if (selectedMonth == null) {
-                            return;
-                          }
-                          model.setSelectedMonth(int.parse(selectedMonth));
-                          DateTime now = DateTime.now();
-                          model.init(
-                              DateTime(now.year, int.parse(selectedMonth), 1));
-                        },
-                        items: _monthList
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                    Container(
-                      child: Text('月'),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            Container(
-              // height: 400,
-              width: size.width,
-              decoration: BoxDecoration(
-                color: Colors.lime,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(10, 10),
-                    color: Theme.of(context).scaffoldBackgroundColor,
-                    blurRadius: 20,
-                  ),
-                  BoxShadow(
-                    offset: Offset(-10, -10),
-                    color: Theme.of(context).scaffoldBackgroundColor,
-                    blurRadius: 20,
-                  ),
-                ],
-              ),
-              margin: const EdgeInsets.fromLTRB(10, 20, 10, 10),
-              child: Column(
+        body: Consumer<AnalyticsProvider>(builder: (ctx, model, _) {
+          return Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Row(
                     children: [
                       Container(
-                        margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                        child: CircleAvatar(
-                          backgroundColor: Colors.white,
-                          radius: 30,
-                          child: Icon(Icons.coffee_outlined),
+                        child: DropdownButton<String>(
+                          value: model.selectedYear.toString(),
+                          onChanged: (String? selectYear) {
+                            if (selectYear == null) {
+                              return;
+                            }
+                            model.setSelectedYear(int.parse(selectYear));
+                            DateTime now = DateTime.now();
+                            model.init(
+                                DateTime(int.parse(selectYear), now.month, 1));
+                          },
+                          items: _yearList
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
                         ),
                       ),
-                      Text(
-                        model.selectedMonth.toString(),
-                        style: const TextStyle(
-                          fontSize: 40,
-                          color: Colors.black,
-                        ),
-                      ),
-                      const Text(
-                        '月の統計',
-                        style: TextStyle(
-                          fontSize: 25,
-                          color: Colors.black,
-                        ),
-                      ),
+                      Container(
+                        child: Text('年'),
+                      )
                     ],
                   ),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      // _item(100, '合計'),
-                      _item(model.homeCount, 'おうち'),
-                      _item(model.shopCount, 'おみせ'),
+                      Container(
+                        child: DropdownButton<String>(
+                          value: model.selectedMonth.toString(),
+                          onChanged: (String? selectedMonth) {
+                            if (selectedMonth == null) {
+                              return;
+                            }
+                            model.setSelectedMonth(int.parse(selectedMonth));
+                            DateTime now = DateTime.now();
+                            model.init(DateTime(
+                                now.year, int.parse(selectedMonth), 1));
+                          },
+                          items: _monthList
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      Container(
+                        child: Text('月'),
+                      )
                     ],
                   ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: const Divider(
-                      color: Colors.black,
-                      thickness: 1.0,
-                    ),
-                  ),
-                  _topCoffeeItem(model),
-                  // _defaultItem('キリマンジャロ', '最も多いブランド'),
-                  // _defaultItem('スタバ', '最も多いカフェ'),
                 ],
               ),
-            ),
-          ],
-        );
-      }),
+              Container(
+                // height: 400,
+                width: size.width,
+                decoration: BoxDecoration(
+                  color: Colors.lime,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(10, 10),
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      blurRadius: 20,
+                    ),
+                    BoxShadow(
+                      offset: Offset(-10, -10),
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      blurRadius: 20,
+                    ),
+                  ],
+                ),
+                margin: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 30,
+                            child: Icon(Icons.coffee_outlined),
+                          ),
+                        ),
+                        Text(
+                          model.selectedMonth.toString(),
+                          style: const TextStyle(
+                            fontSize: 40,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const Text(
+                          '月の統計',
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        // _item(100, '合計'),
+                        _item(model.homeCount, 'おうち'),
+                        _item(model.shopCount, 'おみせ'),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: const Divider(
+                        color: Colors.black,
+                        thickness: 1.0,
+                      ),
+                    ),
+                    _topCoffeeItem(model),
+                    // _defaultItem('キリマンジャロ', '最も多いブランド'),
+                    // _defaultItem('スタバ', '最も多いカフェ'),
+                  ],
+                ),
+              ),
+            ],
+          );
+        }),
+      ),
     );
   }
 }
