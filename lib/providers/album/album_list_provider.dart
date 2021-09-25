fd4c443e17b1370eebbfe06901dc249d9f66162f
yudate <y0303noki@gmail.com>
2021-09-25 16:24:23 +0900

    マイアルバムの説明をつけてみた

--- lib/providers/album/album_list_provider.dart
+++ lib/providers/album/album_list_provider.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_project2/const/common_constant.dart';
 import 'package:coffee_project2/database/coffee_image_firebase.dart';
 import 'package:coffee_project2/model/album_model.dart';
 import 'package:coffee_project2/model/coffee_image_model.dart';
@@ -13,6 +14,16 @@ class AlbumListProvider extends ChangeNotifier {
   List<CoffeeImageModel> _coffeeImageModels = [];
   List<CoffeeImageModel> get coffeeImageModels => _coffeeImageModels;
 
+  // 説明を開く
+  bool _descriptionShowing = false;
+  bool get descriptionShowing => _descriptionShowing;
+  set descriptionShowing(bool e) {
+    _descriptionShowing = e;
+    notifyListeners();
+  }
+
+  int limitCount = 0;
+
   CoffeeImageFirebase _coffeeImageDb = CoffeeImageFirebase();
 
   CoffeeImageModel findById(String id) {
@@ -23,6 +34,11 @@ class AlbumListProvider extends ChangeNotifier {
   Future findAlbumDatas() async {
     _coffeeImageModels = [];
     _coffeeImageModels = await _coffeeImageDb.fetchCoffeeImageDatas();
+    limitCount =
+        CommonConstant.NORMAL_MEMBER_ALBUM_LIMIT - _coffeeImageModels.length;
+    if (limitCount < 0) {
+      limitCount = 0;
+    }
     notifyListeners();
   }
 }
